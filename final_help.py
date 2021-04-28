6e1f657f9fb7c549cf63a7662f1b6337106299f8
cemisbond <83353309+cemisbond@users.noreply.github.com>
2021-04-28 09:36:33 -0400

    Add files via upload

---
+++ final_help.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import pydeck as pdk
+import mapbox as mb
+import streamlit as st
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime as dt
+import final_help
+from pycountry_convert import country_alpha2_to_continent_code, country_name_to_country_alpha2
+
+def read_data(fileName):
+    df = pd.read_csv(fileName)
+    file_dict = {}
+    file_details =[]
+    columns = [ 'Feet', 'Year', 'Type', 'Main use', 'Country', 'City','lat','lon']
+    for i, row in df.iterrows():
+        sub_list = []
+        for col in columns:
+            col_no = df.columns.get_loc(col)
+            sub_list.append(row[col_no])
+        file_details.append(sub_list)
+    item = 0
+    build_name = df['Name']
+    for items in file_details:
+        file_dict[build_name[item]] = file_details[item]
+        item +=1
+    return df, file_dict
+
+def get_continent(col):
+    try:
+        countryName = str(col).strip()
+        cn_a2_code =  country_name_to_country_alpha2(countryName)
+    except Exception as e:
+        print(e)
+        cn_a2_code = 'Unknown'
+    try:
+        cn_continent = country_alpha2_to_continent_code(cn_a2_code)
+    except Exception as e:
+        print(e)
+        cn_continent = 'Unknown'
+    return cn_continent
+
+def world_map(df):
+    locations = []
+    columns = ['Name','lat','lon']
+    for i, row in df.iterrows():
+        sub_list = []
+        for col in columns:
+            col_no = df.columns.get_loc(col)
+            sub_list.append(row[col_no])
+        locations.append(sub_list)
+    map_df = pd.DataFrame(locations, columns =['Name','lat','lon'])
+
+    ICON_URL = "https://upload.wikimedia.org/wikipedia/commons/f/f2/Building_icon.png"
+    icon_data = {
+        "url": ICON_URL,
+        "width": 256,
+        "height": 256,
+        "anchorY": 256,
+    }
+    map_df["icon_data"] = None
+    for i in map_df.index:
+        map_df["icon_data"][i] = icon_data
+
+    view_state = pdk.ViewState(
+        latitude=0,
+        longitude=0,
+        zoom = 0,
+        pitch = 0)
+
+    layer = pdk.Layer('IconLayer',
+                      data = map_df,
+                      get_position='[lon, lat]',
+                      get_icon="icon_data",
+                      #get_radius = 100,
+                      get_size =10,
+                      get_scale = 100,
+                      get_color=[255,0,255],
+                      pickable=True
+                      )
+    #tool_tip = {'html': 'Name: <br/> {Name}', 'style': {'backgroundColor': 'steelblue', 'color': 'white'}}
+
+    map = pdk.Deck( map_style='mapbox://styles/mapbox/light-v9' , initial_view_state = view_state, layers = [layer], tooltip = {"text": "{Name}"})
+
+    st.pydeck_chart(map)
+
+def freq_dict_function(dict, select):
+    dict_freq = {}
+
+    for k in dict.keys():
+        if dict[k][select] not in dict_freq.keys():
+            item = dict[k][select]
+            dict_freq[item] = 1
+        else:
+            item = dict[k][select]
+            dict_freq[item] +=1
+
+    return dict_freq
+
+def sort_dict_func(dict):
+    frequency = sorted(dict.values(), reverse= True)
+    sorted_dict = {}
+    for f in frequency:
+        for x in dict.keys():
+            if dict[x] == f:
+                sorted_dict[x] = dict[x]
+                dict.pop(x)
+                break
+    sorted_key = [key for key in sorted_dict.keys()]
+    sorted_freq = [freq for freq in sorted_dict.values()]
+    return sorted_key, sorted_freq
+
+def bar_chart_plot(x,y,select,primary,secondary,grid_color='r',grid_style=''):
+    fig, ax = plt.subplots()
+    width = 0.4
+    ax.bar(x, y, width = width , align = 'center', color = primary, linewidth = width*2, edgecolor = secondary)
+    #Plot Features
+    plt.title(f'Bar Chart of Number of Buildings according to {select}')
+    plt.ylabel('Number of Buildings')
+    plt.xlabel(select)
+    plt.xticks(rotation = 90 )
+    plt.grid(color = grid_color, linestyle = grid_style, linewidth = 0.5)
+
+    return plt
+
+def pie_chart(sizes,list,num):
+    fig, ax = plt.subplots()
+    explode_val = [0,0,0,0,0,0,0]
+    explode_val[num] = 0.2
+    ax.pie(sizes, labels = list,explode = explode_val, autopct='%1.1f%%', startangle=60)
+    ax.axis('equal')
+    plt.title("Percentage of Buildings according to type", fontweight = 'bold', fontsize = 10)
+    return plt
+
---
+++ final_project_cem.py
@@ -0,0 +1,100 @@
+"""
+Name:       Your Name
+CS230:      Section XXX
+Data:       Which data set you used
+URL:        Link to your web application online (see extra credit)
+
+Description:
+
+This program analyzes the Skyscrapers.csv file. It creates a world map with all of the values using pydeck to be shown on Streamlit.
+Next it focuses on making a bar chart taking user input from Streamlit and allows user to customize the chart greatly
+Followed by the bar chart, users are allowed to investigate the different type of buildings and it creates a pie chart with the selected value exploded for easier view.
+
+"""
+import pandas as pd
+import pydeck as pdk
+import mapbox as mb
+import streamlit as st
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime as dt
+import final_help
+from pycountry_convert import country_alpha2_to_continent_code, country_name_to_country_alpha2
+
+#Data Frame
+#File Dictionary (Key - Name : Values List - [Feet, Year, Type, Main use, Country, City,lat,lon])
+df, file_dict = final_help.read_data('skyscrapers.csv')
+
+#Title
+st.title(f"Final Project Cem Ozsumer \n Skyscrapers Around the World")
+
+#Continent Addition to Each Item
+i = 0
+country_list = []
+for country in df.Country:
+    a = final_help.get_continent(country)
+    country_list.append(a)
+
+    df.loc[i,'Continent'] = a
+    i += 1
+num = 0
+for k in file_dict.keys():
+        file_dict[k].append(country_list[num])
+        num+=1
+
+#MAP
+final_help.world_map(df)
+
+#PLOT 1
+st.header("Plot Time!")
+options = { "Country": 4,  "Continent": 8, "Year": 1}
+left_column, right_column = st.beta_columns(2)
+with left_column:
+    select = st.radio("Pick an option to use in bar chart: ",list(options.keys()))
+with right_column:
+    primary = st.color_picker(label = "Pick your primary color")
+    secondary = st.color_picker(label = "Pick your secondary color")
+
+bar_info = final_help.freq_dict_function(file_dict, options[select])
+sorted_bar_key, sorted_bar_value = final_help.sort_dict_func(bar_info)
+
+grid = st.checkbox(label = "Would you like plot grid?")
+if grid:
+    left_column, right_column = st.beta_columns(2)
+    with left_column:
+        grid_color = st.color_picker(label = "Pick your grid color")
+    with right_column:
+
+        values = {'Line':'-','Dashed':'--','Dot-dash':'-.','Dotted':':'}
+        user_grid = st.selectbox("Pick an option to use in bar chart: ",options = list(values.keys()))
+        grid_style = values[user_grid]
+    st.pyplot(final_help.bar_chart_plot(sorted_bar_key, sorted_bar_value,select,primary,secondary,grid_color,grid_style))
+else:
+    st.pyplot(final_help.bar_chart_plot(sorted_bar_key, sorted_bar_value,select,primary,secondary))
+
+#PLOT 2
+st.header("Building Type & Pie Chart")
+def filterType(df,x):
+    return df[df['Type'] == x]
+
+build_list = list(df.Type.unique())
+type_select = st.selectbox(label ="Type of Building", options=build_list)
+df_type = filterType(df, type_select)
+st.dataframe(df_type)
+
+build_num = build_list.index(type_select)
+
+type_all_list = list(df.groupby('Type')['Type'].count())
+type_all_list.sort(reverse = True)
+
+st.pyplot(final_help.pie_chart(type_all_list,build_list,build_num))
+
+'''
+def count_build(df, group):
+    cont_list = []
+    for continent in df.groupby(group)['Name'].count():
+        cont_list.append(continent)
+
+    return cont_list
+'''
